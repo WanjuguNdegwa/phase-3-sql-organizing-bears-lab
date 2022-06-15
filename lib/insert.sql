0a7b65e63cb983bdc73f54a47bea723a50258a96
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-06-15 12:36:25 +0300

    commit tree 24e25ab922979b56210891cbff952eedbfb51b0a

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 8, 'M', 'brown', 'happy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 5, 'M', 'yellow', 'chipper', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 1, 'F', 'white', 'angry', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergent Brown', 4, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 10, 'F', 'white', 'laid back', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 3, 'M', 'yellow', 'sad', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 2, 'F', 'black', 'sleepy', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 7, 'M', 'brown', 'excited', 1);
